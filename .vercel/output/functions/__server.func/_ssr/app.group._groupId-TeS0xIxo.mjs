e36b5e844548cf785b0c97136f7318ee8f8989eb
Yash Bajaj <bajajyash06@gmail.com>
2026-07-17 12:43:39 +0530

    Ignore .vercel directory

--- .vercel/output/functions/__server.func/_ssr/app.group._groupId-TeS0xIxo.mjs
+++
@@ -1,6 +0,0 @@
-import { m as createFileRoute, p as lazyRouteComponent } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/app.group._groupId-TeS0xIxo.js
-var $$splitComponentImporter = () => import("./app.group._groupId-ThQKoN86.mjs");
-var Route = createFileRoute("/app/group/$groupId")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
